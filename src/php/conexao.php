f2801b2ae41d884d9203f631a7e5c20990b90018
Gabriel Severino <gabrielferreiraseverino@gmail.com>
2025-02-20 15:52:08 -0300

    publish_crud bootstrap+php+mysql

---
+++ src/php/conexao.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST", "127.0.0.1");
+define("USER", "root");
+define("PASS", "");
+define("DB", "crud");
+
+$conexao = mysqli_connect(HOST, USER, PASS, DB);
+?>
---
+++ src/php/navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-md">
+        <a class="navbar-brand" href="../../index.php">CRUD - Gabrielsev</a>
+    </div>
+</nav>
